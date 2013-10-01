a9b3c1538ea6791a5b92110652e1bd71994c0f8c
Kazuki Suda <kazuki.suda@gmail.com>
2013-10-01 17:39:41 +0900

    Fix to can't show img tag when "src" isn't url
    Binary image converts to embedding base64 image.

---
+++ image2base64.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+html=`cat /dev/stdin`
+images=''
+
+IFS=$'\n'
+for line in $html; do
+    matches=`echo "$line" | egrep -o 'src=".*?"' | sed -e 's/src="\(.*\)"/\1/g' | egrep -v '^https?|js$|css$'`
+    images="$images $matches"
+done
+
+for image in `echo "$images" | tr ' ' '\n' | sort | uniq`; do
+    test ! -e "$image" && continue
+
+    ext=`echo "$image" | grep -o '\w\+$'`
+    base64=`cat "$image" | base64 | tr -d '\n'`
+    html=`echo "$html" | sed -e 's#src="'$image'"#src="data:image/'$ext';base64,'$base64'"#g'`
+done
+echo $html
--- plugin/quickrun-markdown-gfm.vim
+++ plugin/quickrun-markdown-gfm.vim
@@ -7,5 +7,5 @@ if !exists('g:quickrun_config')
 endif
 
 let g:quickrun_config['markdown/gfm'] = {
-\   'exec': 'curl -X POST -H "Content-Type: text/plain" --data-binary \@%s -s '. g:quickrun_markdown_gfm_github_api_url . '/markdown/raw | ' . expand('<sfile>:p:h') . '/../render.sh'
+\   'exec': 'curl -X POST -H "Content-Type: text/plain" --data-binary \@%s -s '. g:quickrun_markdown_gfm_github_api_url . '/markdown/raw | ' . expand('<sfile>:p:h') . '/../image2base64.sh | ' . expand('<sfile>:p:h') . '/../render.sh'
 \ }
